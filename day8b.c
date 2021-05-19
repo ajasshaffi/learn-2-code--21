8cdb8c45ecede50553e957e88172bea59880cacd
ajasshaffi <83956432+ajasshaffi@users.noreply.github.com>
2021-05-19 22:22:58 +0530

    Update day8b.c

--- day8b.c
+++ day8b.c
@@ -12,37 +12,24 @@
 #include <stdlib.h>
 
 int main() {
-    int a,b,i;
-        /*printf("Enter the option number :");*/
-    scanf("%d",&a);
-    /*printf("Enter the next option number :");*/
-    scanf("%d",&b);
-        do
-        {
-            printf("\n1.Table of 1");
-            printf("\n2.Table of 2");
-            printf("\n3.Table of 3");
-            printf("\n4.Table of 4");
-            printf("\n5.Table of 5");
-            printf("\n6.Exit\n");
-            if(a<6)
-            {
-            for(i=1;i<=10;i++)
-                printf("%d ",a*i);
-            }
-            else
-            {
-                printf("Enter a valid option!");
-            }
-        }
-         while(b!=6);
+    int a,i;
+do
+{
+    printf("\n1.Table of 1");
+    printf("\n2.Table of 2");
+    printf("\n3.Table of 3");
+    printf("\n4.Table of 4");
+    printf("\n5.Table of 5");
+    printf("\n6.Exit\n");
+    if(a<6)
     {
-        printf("\n1.Table of 1");
-        printf("\n2.Table of 2");
-        printf("\n3.Table of 3");
-        printf("\n4.Table of 4");
-        printf("\n5.Table of 5");
-        printf("\n6.Exit");
+        for(i=1;i<=10;i++)
+            printf("%d",a*i);
     }
+    else if(a>6)
+    {
+        printf("Enter a valid option!");
+    }
+    while(a!=6);
     return 0;
 }
