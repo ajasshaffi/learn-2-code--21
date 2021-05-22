716c02bc1ca087de746f15c45959a65036e2d1dc
ajasshaffi <83956432+ajasshaffi@users.noreply.github.com>
2021-05-22 22:30:46 +0530

    Create day11a.c

---
+++ day11a.c
@@ -0,0 +1,28 @@
+Write a C program to read n values from the user and store it in the array. Reprint the values stored in the array and also print the value of second element in the array.
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int numbers[10],i,a;
+
+    /*printf("Enter the numbers of value :");*/
+    scanf("%d",&a);
+    /*printf("Enter the elements :");*/
+    for(i=0;i<=a-1;i++)
+    {
+        scanf("%d",&numbers[i]);
+ 
+    }
+    printf("Entered array:");
+    for(i=0;i<=a-1;i++)
+    {
+        printf(" %d",numbers[i]);
+    }
+        printf("\n%d",numbers[1]);
+    
+    return 0;
+}
