063fe642f1d4973f402f08d641879eded1cc1707
ajasshaffi <83956432+ajasshaffi@users.noreply.github.com>
2021-05-26 22:56:26 +0530

    Create day14a.c

---
+++ day14a.c
@@ -0,0 +1,35 @@
+//Write a C Program to read a 2D Array and then display it.
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a[10][10],r,c,i,j;
+
+    /*printf("Enter the no of rows :");*/
+    scanf("%d",&r);
+    /*printf("Enter the no coloumns :");*/
+    scanf("%d",&c);
+    /*printf("Enter the array elements");*/
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+  
+ 
+    return 0;
+}
